519ebde362b2220d262889378f1f012cf5ac0f5e
ydhstrs <yudhatriya07@gmail.com>
2022-10-24 19:05:19 +0700

    Fix Create Asset Image Error

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,41 +10,71 @@ use Illuminate\Validation\Rules;
 
 class AdminController extends Controller
 {
- 
-    public function dashboard(){
-        return view('dashboard',['hotels'=>Hotel::all()]);
+    public function dashboard()
+    {
+        return view('dashboard', ['hotels' => Hotel::all()]);
     }
 
-    public function index(){
+    public function index()
+    {
         return view('users.index', [
-            'users' => User::whereNotNull('id_hotel')->get()
+            'users' => User::whereNotNull('id_hotel')->get(),
         ]);
     }
 
-    public function create(){
-        return view('users.create',[
-            'hotels' => Hotel::all()
+    public function create()
+    {
+        return view('users.create', [
+            'hotels' => Hotel::all(),
         ]);
     }
 
-
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
         ]);
 
-
-       User::create([
+        User::create([
             'name' => $request->name,
             'email' => $request->email,
             'role' => 0,
-            'id_hotel'=>$request->id_hotel,
+            'id_hotel' => $request->id_hotel,
             'password' => Hash::make($request->password),
         ]);
 
         return redirect('/dashboard/user')->with('success', 'Berhasil Menambahkan User Baru');
-        
+    }
+    public function edit(User $user)
+    {
+        return view('users.edit', [
+            'user' => $user,
+            'hotels' => Hotel::all(),
+        ]);
+    }
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'id_hotel' => ['required'],
+        ]);
 
+        User::where('id', $user->id)->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role' => 0,
+            'id_hotel' => $request->id_hotel,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect('/dashboard/user')->with('success', 'Berhasil Mengubah User');
+    }
+    public function destroy(User $user)
+    {
+        User::destroy($user->id);
+
+        return redirect('/dashboard/user')->with('delete', 'Isi Materi Baru Telah Dihapus');
     }
 }
