c3f87e254fe686183d5ca2ffe18bb11bb04609b3
arybobysiregar <aryboby1@gmail.com>
2022-10-31 19:47:07 +0700

    add struk

---
+++ resources/views/employee/struk.blade.php
@@ -0,0 +1,104 @@
+@extends('employee/layouts/app')
+
+@section('contents')
+    <div>
+        <button class="bg-blue-500 text-white p-2 rounded" onclick="printDiv('printableArea')">Print Struk</button>
+    </div>
+
+    <div class="mt-10 bg-white p-4" id="printableArea">
+
+        <div class="w-full h-[0.01rem] bg-black mb-2"></div>
+        {{-- Header --}}
+        <div class="flex gap-10">
+            <img src="{{ asset('images/LOGO_DENATIO.png') }}" class="w-24">
+            <div>
+                <h1 class="text-4xl font-bold">{{ $book->hotel->name }}</h1>
+                <h4 class="text-lg">Kwitansi Pembayaran</h4>
+                <h4 class="text-sm">Jalan binjai kota binjai nomor telepon (0821313049)</h4>
+            </div>
+        </div>
+
+        <div class="w-full h-[0.01rem] bg-black my-2"></div>
+        {{-- Body --}}
+        <div class="flex justify-between mt-4">
+            {{-- Left --}}
+            <div class="flex justify-between gap-4">
+                <div class="flex-col">
+                    <h4>No Transaksi</h4>
+                    <h4>Nama Tamu</h4>
+                </div>
+                <div class="flex-col">
+                    <h4>:</h4>
+                    <h4>:</h4>
+                </div>
+                <div class="flex-col">
+                    <h4>{{$book->nota}}</h4>
+                    <h4>{{ $book->guestname }}</h4>
+                </div>
+            </div>
+            {{-- center --}}
+            <div class="flex justify-between gap-4">
+                <div class="flex-col">
+                    <h4>Kamar</h4>
+                    <h4>CheckIn</h4>
+                    <h4>CheckOut</h4>
+                </div>
+                <div class="flex-col">
+                    <h4>:</h4>
+                    <h4>:</h4>
+                    <h4>:</h4>
+                </div>
+                <div class="flex-col">
+                    <h4>{{ $book->nameroom->name }}</h4>
+                    <h4>{{ $book->checkin }}</h4>
+                    <h4>{{ $book->checkout }}</h4>
+                </div>
+            </div>
+
+            {{-- Right --}}
+            <div class="flex justify-between gap-4">
+                <div class="flex-col">
+                    <h4>Harga Kamar</h4>
+                    <h4>Jumlah Hari</h4>
+                    <h4>Total</h4>
+                </div>
+                <div>
+                    <h4>:</h4>
+                    <h4>:</h4>
+                    <h4>:</h4>
+                </div>
+                <div class="flex-col">
+                    <h4>Rp. {{ number_format($book->nameroom->price) }},-</h4>
+                    <h4>{{ $book->days }}</h4>
+                    <h4>Rp. {{ number_format($book->days * $book->nameroom->price) }},-</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full flex justify-end my-12">
+            <div>
+                <h4>{{ $book->book_date }}</h4>
+                <br>
+                <br>
+                <br>
+                <h4>{{ Auth::user()->name }}</h4>
+            </div>
+        </div>
+
+        <div class="w-full h-[0.01rem] bg-black my-2"></div>
+
+    </div>
+@endsection
+
+<script>
+    function printDiv(divName) {
+        var printContents = document.getElementById(divName).innerHTML;
+        var originalContents = document.body.innerHTML;
+
+        document.body.innerHTML = printContents;
+
+        window.print();
+
+        document.body.innerHTML = originalContents;
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -68,4 +68,6 @@ Route::middleware('auth')->group(function () {
 
     Route::get('hotel/rooms', [\App\Http\Controllers\HotelController::class, 'rooms'])->name('hotel.rooms');
     Route::get('hotel/shift', [\App\Http\Controllers\HotelController::class, 'shift'])->name('hotel.shift');
+  
+    Route::get('hotel/struk/{id}', [\App\Http\Controllers\HotelController::class, 'struk'])->name('hotel.struk');
 });
