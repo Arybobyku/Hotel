ce108e03edba764675a8a4ec4e2b4b316075dd72
ydhstrs <yudhatriya07@gmail.com>
2025-03-08 23:42:45 +0700

    Add Falback Route Under Cons

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// Fallback route
+Route::fallback(function () {
+    return redirect('/');
+});
 
 require __DIR__.'/auth.php';
 
